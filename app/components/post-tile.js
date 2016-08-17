f40fe6fcb8d0d3739d384fcda8200a14e663a045
Leah Nelson and Matt Wilkin <leah@>
2016-08-17 16:38:13 -0700

    start sorting by date

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // posts: Ember.computed.sort('posts', 'sortDefinition'),
+  // sortDefinition: ['date'],
+  //www.codeschool.com/blog/2016/03/21/dynamic-sorting-in-emberjs/
+
   actions: {
     update(post, params) {
       this.sendAction('update', post, params);
